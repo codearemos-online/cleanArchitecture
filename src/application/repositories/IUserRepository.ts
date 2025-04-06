412fa846ffa7c7a90be46969fda77b57c53873d7
codearemos-online <dev@codearemos.online>
2025-04-06 14:37:58 -0500

    feat(user): getAll users from a local array

--- src/application/repositories/IUserRepository.ts
+++ src/application/repositories/IUserRepository.ts
@@ -1,7 +1,7 @@
 import { User } from "../../domain/entities/User";
 
 export interface IUserRepository{
-    //getAll():Promise<User[] | null>;
+    getAll():Promise<User[] | null>;
     getById(id:string):Promise<User | null>;
     //create(user:User):Promise<User>;
     //update(user:User):Promise<User>;
