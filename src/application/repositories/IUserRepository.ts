40f17d691403c515c6e35eef9b0d3ba7c7d69f45
codearemos-online <dev@codearemos.online>
2025-04-06 13:29:51 -0500

    feat(user): implement user module configuration

---
+++ src/application/repositories/IUserRepository.ts
@@ -0,0 +1,9 @@
+import { User } from "../../domain/entities/User";
+
+export interface IUserRepository{
+    //getAll():Promise<User[] | null>;
+    getById(id:string):Promise<User | null>;
+    //create(user:User):Promise<User>;
+    //update(user:User):Promise<User>;
+    //delete(id:string):Promise<void>;
+}
